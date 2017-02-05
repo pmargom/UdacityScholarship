326c43141a6be8342717bb4289d8406d6b63378d
Pedro Martín <pmargom@gmail.com>
2017-02-05 18:47:08 +0000

    Added toast if there no apps installed to open maps

--- ud851-Exercises-student/Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ ud851-Exercises-student/Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -135,6 +135,8 @@ public class MainActivity extends AppCompatActivity {
         // TODO (4) Verify that this Intent can be launched and then call startActivity. DONE
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        } else {
+            Toast.makeText(this, "No app to open maps.", Toast.LENGTH_LONG).show();
         }
     }
 
