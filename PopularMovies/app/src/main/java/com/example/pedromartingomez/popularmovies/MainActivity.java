38459547e89c1dd45b6c2adae4e60b3041aeccfc
Pedro Martin Gomez <pmargom@gmail.com>
2017-02-11 13:26:26 +0000

    Changes: 	.- Added settings activity 	.- Progressbar centered 	.- Replace AsyncTask by loader

--- PopularMovies/app/src/main/java/com/example/pedromartingomez/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/com/example/pedromartingomez/popularmovies/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.pedromartingomez.popularmovies;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,6 +19,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
 
 import com.example.pedromartingomez.popularmovies.utilities.MovieDBJsonUtils;
 import com.example.pedromartingomez.popularmovies.utilities.NetworkUtils;
@@ -19,7 +27,10 @@ import com.example.pedromartingomez.popularmovies.models.Movie;
 
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHnadler {
+public class MainActivity extends AppCompatActivity
+        implements MovieAdapter.MovieAdapterOnClickHnadler,
+        LoaderCallbacks<Movie[]>,
+        SharedPreferences.OnSharedPreferenceChangeListener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView mRecyclerView;
@@ -28,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
     private TextView mErrorMessageDisplay;
 
     private ProgressBar mLoadingIndicator;
+    private static final int MOVIE_LOADER_ID = 0;
+    private static boolean PREFERENCES_HAVE_BEEN_UPDATED = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,11 +51,8 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
 
-        //LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        //mRecyclerView.setLayoutManager(layoutManager);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         mRecyclerView.setLayoutManager(gridLayoutManager);
-        //mRecyclerView.setHasFixedSize(true);
 
         mMovieAdapter = new MovieAdapter(this);
 
@@ -50,7 +61,42 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         /* Once all of our views are setup, we can load the weather data. */
-        loadMovieData();
+        //loadMovieData();
+        int loaderId = MOVIE_LOADER_ID;
+
+        LoaderCallbacks<Movie[]> callback = MainActivity.this;
+
+        Bundle bundleForLoader = null;
+
+        getSupportLoaderManager().initLoader(loaderId, bundleForLoader, callback);
+
+        Log.d(TAG, "onCreate: registering preference changed listener");
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (PREFERENCES_HAVE_BEEN_UPDATED) {
+            Log.d(TAG, "onStart: preferences were updated");
+            getSupportLoaderManager().restartLoader(MOVIE_LOADER_ID, null, this);
+            PREFERENCES_HAVE_BEEN_UPDATED = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        /* Unregister MainActivity as an OnPreferenceChangedListener to avoid any memory leaks. */
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        PREFERENCES_HAVE_BEEN_UPDATED = true;
     }
 
     private void showMovieDataView() {
@@ -67,69 +113,124 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
-    private void loadMovieData() {
-        showMovieDataView();
-        new FetchMovieTask().execute();
-    }
-
     @Override
     public void onClick(Movie movie) {
         // TODO: Load DetailActivity
         Toast.makeText(this, movie.getTitle(), Toast.LENGTH_LONG).show();
     }
 
-    public class FetchMovieTask extends AsyncTask<String, Void, Movie[]> {
+    @Override
+    public Loader<Movie[]> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<Movie[]> (this) {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-        }
+            Movie[] mMovieData = null;
 
-        @Override
-        protected Movie[] doInBackground(String... params) {
-            //if (params.length == 0) return null;
-
-            //String sortValue = params[0];
-            // TODO: Prepare NetworkUtils
-            URL movieRequestUrl = NetworkUtils.buildUrl(MainActivity.this, "", "");
-
-            try {
-                // TODO: Prepare MovieDbJsonUtils
-                String jsonMoviesResponse = NetworkUtils
-                        .getResponseFromHttpUrl(movieRequestUrl);
-
-                Movie[] jsonMoviesData = MovieDBJsonUtils
-                        .getFullMoviesDataFromJson(MainActivity.this, jsonMoviesResponse);
-
-                return jsonMoviesData;
-
-                /*Movie[] movies = new Movie[15];
-                for (int i = 0; i < 15; i++) {
-                    Movie movie = new Movie();
-                    movie.setTitle("Lo que el viento se llevó");
-                    movies[i] = movie;
+            @Override
+            protected void onStartLoading() {
+                if (mMovieData != null) {
+                    deliverResult(mMovieData);
+                } else {
+                    mLoadingIndicator.setVisibility(View.VISIBLE);
+                    forceLoad();
                 }
-                return movies;
-                */
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
             }
-        }
 
-        @Override
-        protected void onPostExecute(Movie[] movieData) {
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (movieData != null) {
-                showMovieDataView();
-                mMovieAdapter.setMoviesData(movieData);
-            } else {
-                showErrorMessage();
+            @Override
+            public Movie[] loadInBackground() {
+                URL movieRequestUrl = NetworkUtils.buildUrl(MainActivity.this, "", "");
+
+                try {
+                    String jsonMoviesResponse = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+
+                    Movie[] jsonMoviesData = MovieDBJsonUtils.getFullMoviesDataFromJson(MainActivity.this, jsonMoviesResponse);
+
+                    return jsonMoviesData;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
+
+            public void deliverResult(Movie[] data) {
+                mMovieData = data;
+                super.deliverResult(data);
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Movie[]> loader, Movie[] data) {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        mMovieAdapter.setMoviesData(data);
+        if (null == data) {
+            showErrorMessage();
+        } else {
+            showMovieDataView();
         }
     }
+
+    @Override
+    public void onLoaderReset(Loader<Movie[]> loader) {
+
+    }
+
+    private void invalidateData() {
+        mMovieAdapter.setMoviesData(null);
+    }
+
+//    public class FetchMovieTask extends AsyncTask<String, Void, Movie[]> {
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            mLoadingIndicator.setVisibility(View.VISIBLE);
+//        }
+//
+//        @Override
+//        protected Movie[] doInBackground(String... params) {
+//            //if (params.length == 0) return null;
+//
+//            //String sortValue = params[0];
+//            // TODO: Prepare NetworkUtils
+//            URL movieRequestUrl = NetworkUtils.buildUrl(MainActivity.this, "", "");
+//
+//            try {
+//                // TODO: Prepare MovieDbJsonUtils
+//                String jsonMoviesResponse = NetworkUtils
+//                        .getResponseFromHttpUrl(movieRequestUrl);
+//
+//                Movie[] jsonMoviesData = MovieDBJsonUtils
+//                        .getFullMoviesDataFromJson(MainActivity.this, jsonMoviesResponse);
+//
+//                return jsonMoviesData;
+//
+//                /*Movie[] movies = new Movie[15];
+//                for (int i = 0; i < 15; i++) {
+//                    Movie movie = new Movie();
+//                    movie.setTitle("Lo que el viento se llevó");
+//                    movies[i] = movie;
+//                }
+//                return movies;
+//                */
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                return null;
+//            }
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Movie[] movieData) {
+//            mLoadingIndicator.setVisibility(View.INVISIBLE);
+//            if (movieData != null) {
+//                showMovieDataView();
+//                mMovieAdapter.setMoviesData(movieData);
+//            } else {
+//                showErrorMessage();
+//            }
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -143,10 +244,17 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         int id = item.getItemId();
 
         if (id == R.id.action_refresh) {
-            mMovieAdapter.setMoviesData(null);
-            loadMovieData();
+            invalidateData();
+            getSupportLoaderManager().restartLoader(MOVIE_LOADER_ID, null, this);
+            return true;
+        }
+
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
